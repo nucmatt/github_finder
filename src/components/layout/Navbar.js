77cec0a17c611045d10b6fbeb4f53fff27648b9b
Matthew Wessel <nucmatt@gmail.com>
2020-06-16 10:56:43 -0500

    Refactored Navbar and UserItem into stateless functional components.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-	// Default props can be overidden by adding the property to the component/element as an attribute and specifying the new property value.
-	static defaultProps = {
-		title: 'Github Finder',
-		icon: 'fab fa-github',
-	};
-    // PropTypes are basically type checking for your properties so that React will ensure the property value is of the type you specify. In this case, title and icon must be strings AND are required to be entered. NOTE: PropTypes must be imported as above to function.
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		icon: PropTypes.string.isRequired,
-	};
+// More destructuring! Remember that props is implicit within the parentheses that creates the stateless functional component. See UserItem for more destructuring explanation/goodness.
+const Navbar = ({ icon, title}) => {
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={icon} /> {title}
+			</h1>
+		</nav>
+	);
+};
 
-	render() {
-		return (
-			<nav className='navbar bg-primary'>
-				<h1>
-					<i className={this.props.icon}></i> {this.props.title}
-				</h1>
-			</nav>
-		);
-	}
-}
+Navbar.defaultProps = {
+	title: 'Github Finder',
+	icon: 'fab fa-github',
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired,
+};
+// Below is the code for the Navbar as a stateless component. It is refactored above as a stateless functional component since state is not managed in this component.
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+// export class Navbar extends Component {
+// 	// Default props can be overidden by adding the property to the component/element as an attribute and specifying the new property value.
+// 	static defaultProps = {
+// 		title: 'Github Finder',
+// 		icon: 'fab fa-github',
+// 	};
+//     // PropTypes are basically type checking for your properties so that React will ensure the property value is of the type you specify. In this case, title and icon must be strings AND are required to be entered. NOTE: PropTypes must be imported as above to function.
+// 	static propTypes = {
+// 		title: PropTypes.string.isRequired,
+// 		icon: PropTypes.string.isRequired,
+// 	};
+
+// 	render() {
+// 		return (
+// 			<nav className='navbar bg-primary'>
+// 				<h1>
+// 					<i className={this.props.icon}></i> {this.props.title}
+// 				</h1>
+// 			</>
+// 		);
+// 	}
+// }
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,49 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-	// Generally constructors are not used for state UNLESS you have another reason to be creating a constructor. For state only, just use the variable name state as below.
-	// constructor() {
-	// 	// Must call the function super() to call the parent class constructor
-	// 	super();
-	// 	// State is really just a JS object
-	// 	this.state = {
-	// 		id: 'id',
-	// 		login: 'mojombo',
-	// 		avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-	// 		html_url: 'https://github.com/mojombo',
-	// 	};
-	// }
+// Here we extend simply move the destructuring of properties (to minimize code written within the component) into the definition of the stateless functional component by replacing the props keyword with the actual user property destructuring. Remember that if this destructuring is not used every place you use the property you have to write props.user.avatar_url etc.
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+	// const { login, avatar_url, html_url } = props.user;
 
-	// Now that we are passing the UserItem component into the Users component, we no longer need state. Instead we use the UserItem props(properties) to pass in to the Users state for updating and display.
-	// state = {
-	// 	id: 'id',
-	// 	login: 'mojombo',
-	// 	avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-	// 	html_url: 'https://github.com/mojombo',
-	// };
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt=''
+				className='round-img'
+				style={{ width: '60px ' }}
+			/>
+			<h3>{login}</h3>
 
-	render() {
-		// this const is a destructuring statement used so that you don't have to type this.state.login and then this.state.avatar_url and so forth, you can simple type the state property name you want.
-		// const { login, avatar_url, html_url } = this.state;
-		// Refactored to use the Users component. Now we are passing properties into the Users component from here, UserItem. Now every user pulled in from the API will create a UserItem component that is then placed within the Users parent component and from there, all users are placed within the App component to be rendered to the DOM.
-		const { login, avatar_url, html_url } = this.props.user;
-		return (
-			<div className='card text-center'>
-				<img
-					src={avatar_url}
-					alt=''
-					className='round-img'
-					// inline styles like this must be enclosed in double curly braces, properties must be typed in camelCase, and property values must be enclosed as strings.
-					style={{ width: '60px' }}
-				/>
-				<h3>{login}</h3>
-
-				<div>
-					<a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-				</div>
+			<div>
+				<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					More
+				</a>
 			</div>
-		);
-	}
+		</div>
+	);
+};
+
+// Since user has many properties, you can make the user property required as an object. This doesn't make the properties WITHIN user required but makes a user property object required. 
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
 }
+// Below is considered a stateless component. It is refactored (above) into a stateless functional component since it has no state that needs to be maintained.
+// import React, { Component } from 'react';
+// class UserItem extends Component {
+// Generally constructors are not used for state UNLESS you have another reason to be creating a constructor. For state only, just use the variable name state as below.
+// constructor() {
+// 	// Must call the function super() to call the parent class constructor
+// 	super();
+// 	// State is really just a JS object
+// 	this.state = {
+// 		id: 'id',
+// 		login: 'mojombo',
+// 		avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+// 		html_url: 'https://github.com/mojombo',
+// 	};
+// }
+
+// Now that we are passing the UserItem component into the Users component, we no longer need state. Instead we use the UserItem props(properties) to pass in to the Users state for updating and display.
+// state = {
+// 	id: 'id',
+// 	login: 'mojombo',
+// 	avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+// 	html_url: 'https://github.com/mojombo',
+// };
+
+// 	render() {
+// 		// this const is a destructuring statement used so that you don't have to type this.state.login and then this.state.avatar_url and so forth, you can simple type the state property name you want.
+// 		// const { login, avatar_url, html_url } = this.state;
+// 		// Refactored to use the Users component. Now we are passing properties into the Users component from here, UserItem. Now every user pulled in from the API will create a UserItem component that is then placed within the Users parent component and from there, all users are placed within the App component to be rendered to the DOM.
+// 		const { login, avatar_url, html_url } = this.props.user;
+// 		return (
+// 			<div className='card text-center'>
+// 				<img
+// 					src={avatar_url}
+// 					alt=''
+// 					className='round-img'
+// 					// inline styles like this must be enclosed in double curly braces, properties must be typed in camelCase, and property values must be enclosed as strings.
+// 					style={{ width: '60px' }}
+// 				/>
+// 				<h3>{login}</h3>
+
+// 				<div>
+// 					<a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default UserItem;
