e8b27d533447a4a925717ae59577f0e0a84d2eab
Matthew Wessel <nucmatt@gmail.com>
2020-06-18 11:29:39 -0500

    Adding Spinner component and gif. Updated Users component to a functional component and added Spinner component to Users as a child.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
-import UserItem from './UserItem'
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-export class Users extends Component {
+
+// Since Users no longer handles any state, it can be turned in to a functional component. 
+const Users = ({ users, loading }) => {
     // state is now handled within the App component. Users are passed in to this Users component as props from App. You will see in the render method below that the map function now uses this.props.users instead of this.state.users.
     // state = {
     //     users: [
@@ -26,15 +30,24 @@ export class Users extends Component {
     //     ]
     // }
 
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.props.users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        )
-    }
+        if(loading) {
+            return <Spinner />
+        } else {
+            // the render() method is not used for functional components. The functional component returns the JSX directly.
+                // render() {
+                    return (
+                        <div style={userStyle}>
+                            {users.map(user => (
+                                <UserItem key={user.id} user={user} />
+                            ))}
+                        </div>
+                    )
+                }
+        }
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
 }
 
 // This is used to demonstrate that you can use variables to insert styles into your React components in addition to inline styles adn the App.css file. Note the style is an object, camelCase is used for the styles, and the values are enclosed in quotes. All of this is required.
