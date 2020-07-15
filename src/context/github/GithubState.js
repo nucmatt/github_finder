cf4c700442a0faa4848f4ddde092748452077e18
Matthew Wessel <nucmatt@gmail.com>
2020-07-15 11:54:17 -0500

    Found my problem with the app not displaying. I misspelled props.children as props.childrean in GithubState.js. I also wrapped the return statement in parentheses. This error didn't prevent the display but is an error nontheless.

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -3,41 +3,45 @@ import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import {
-    SEARCH_USERS,
-    SET_LOADING,
-    CLEAR_USERS,
-    GET_USER,
-    GET_REPOS
+	SEARCH_USERS,
+	SET_LOADING,
+	CLEAR_USERS,
+	GET_USER,
+	GET_REPOS,
 } from '../types';
 
-const GithubState = props => {
-    const initialState = {
-        users: [],
-        user: {},
-        repos: [],
-        loading: false
-    }
+const GithubState = (props) => {
+	const initialState = {
+		users: [],
+		user: {},
+		repos: [],
+		loading: false,
+	};
 
-    const [state, dispatch] = useReducer(GithubReducer, initialState);
+	const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-    // Search Users
+	// Search Users
 
-    // Get User
+	// Get User
 
-    // Get Repos
+	// Get Repos
 
-    // Clear Users
+	// Clear Users
 
-    // Set Loading
+	// Set Loading
 
-    return <GithubContext.Provider value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading
-    }}>
-        {props.childrean}
-    </GithubContext.Provider>
-}
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				user: state.user,
+				repos: state.repos,
+				loading: state.loading,
+			}}
+		>
+			{props.children}
+		</GithubContext.Provider>
+	);
+};
 
 export default GithubState;
